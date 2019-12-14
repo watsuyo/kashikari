86d245d5e7e504eb583d81a5f94cd474ac333b8e
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2019-12-14 13:16:03 +0900

    fix: organize components (#13)
    * Feature/header (#12)
    
    * header & footer
    
    * fix: title
    
    * fix: fixed
    
    * del: duplicated css

---
+++ src/components/organisms/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="footer">
+    <div class="footer__contents">
+      <div @click="$router.push('/items')">
+        借りる
+      </div>
+      <div @click="$router.push('/post')">
+        貸す
+      </div>
+      <div @click="$router.push('/items')">
+        リスト
+      </div>
+      <div @click="$router.push('/items')">
+        マイページ
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      windowWidth: 0,
+      showDrawer: false
+    }
+  },
+  mounted () {
+    this.calculateWindowWidth()
+    window.addEventListener('resize', this.calculateWindowWidth)
+  },
+  methods: {
+    calculateWindowWidth () {
+      this.windowWidth = window.innerWidth
+      this.showDrawer = this.windowWidth > 1180
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  position: fixed;
+  display: flex;
+  text-align: center;
+  width: 100%;
+  height: $header-height;
+  bottom: 0;
+  left: 0;
+
+  &__contents {
+    position: absolute;
+    display: flex;
+    justify-content: space-evenly;
+    bottom: 20px;
+    width: 100%;
+  }
+}
+</style>
